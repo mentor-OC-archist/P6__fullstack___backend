20d585de6f0f445c64321342d1c1c3c78cdedc64
achicyr <hi.cyril@gmail.com>
2022-06-03 16:53:53 +0900

    PARTIE2: CH1+2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://archist:1&Bigcyri@cluster0.61na4.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 //CORS
 app.use((req, res, next) => {
@@ -8,6 +16,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
 });
 
 app.use('/api/stuff', (req, res, next) => {
